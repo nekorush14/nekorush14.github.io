ed4e0904d2c3ac15b06eba4a545a9b4128e80d10
Mitsuhiro Komuro <applemitkom@gmail.com>
2019-02-16 22:57:44 +0900

    Add blog link to nav bar

--- assets/javascript/applicaiton.js
+++ assets/javascript/applicaiton.js
@@ -5,3 +5,11 @@ $(document).ready(function () {
       $('.sidenav').sidenav('close');
     });
 });
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-134623824-1');
