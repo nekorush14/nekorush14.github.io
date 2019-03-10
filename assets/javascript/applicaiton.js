f856a047e17b7a6ec8f86f6ff99cc7e01cdab766
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2019-03-10 17:55:02 +0900

    Add contact form link

--- assets/javascript/applicaiton.js
+++ assets/javascript/applicaiton.js
@@ -5,11 +5,3 @@ $(document).ready(function () {
       $('.sidenav').sidenav('close');
     });
 });
-
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
-
-gtag('config', 'UA-134623824-1');
